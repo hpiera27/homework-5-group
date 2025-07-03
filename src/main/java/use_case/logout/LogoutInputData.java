cf51e620b61d5d1b5788b1721b4a639231566b6d
Herman Piera <hpiera8@gmail.com>
2025-07-03 18:51:12 -0400

    Herman's parts

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -7,7 +7,11 @@ public class LogoutInputData {
     private String username;
 
     public LogoutInputData(String username) {
+        this.username = username;
         // TODO: save the current username in an instance variable and add a getter.
     }
 
+    public String getUsername() {
+        return username;
+    }
 }

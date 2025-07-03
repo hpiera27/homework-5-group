c05b8511be31e3b56803d9c88e3ca869fb0e1e3a
Herman Piera <hpiera8@gmail.com>
2025-07-03 18:55:18 -0400

    fixed a checkstyle

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -8,7 +8,7 @@ public class LogoutInputData {
 
     public LogoutInputData(String username) {
         this.username = username;
-        // TODO: save the current username in an instance variable and add a getter.
+        // save the current username in an instance variable and add a getter.
     }
 
     public String getUsername() {
--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -9,7 +9,7 @@ public class LogoutInteractor implements LogoutInputBoundary {
 
     public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
                             LogoutOutputBoundary logoutOutputBoundary) {
-        // TODO: save the DAO and Presenter in the instance variables.
+        // save the DAO and Presenter in the instance variables.
         // Which parameter is the DAO and which is the presenter?
         this.userDataAccessObject = userDataAccessInterface;
         this.logoutPresenter = logoutOutputBoundary;
@@ -17,7 +17,7 @@ public class LogoutInteractor implements LogoutInputBoundary {
 
     @Override
     public void execute(LogoutInputData logoutInputData) {
-        // TODO: implement the logic of the Logout Use Case (depends on the LogoutInputData.java TODO)
+        // implement the logic of the Logout Use Case (depends on the LogoutInputData.java)
         // * get the username out of the input data,
         // * set the username to null in the DAO
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
